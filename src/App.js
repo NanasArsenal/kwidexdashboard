aacb5ffd1a2b6dcb02d94fa0eac592c0c9af487e
Nana Kweku <nanakwekudev@gmail.com>
2022-05-21 10:20:37 +0000

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from "./pages/components/Sidebar";
+import Navbar from "./pages/components/Navbar"; 
+import './index.css'
+function App() {
+  return (
+    <h1 className="text-3xl underline">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
---
+++ src/pages/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className='w-78px bg-green-600'>
+        <h1 className='text-5xl'>Kwidex</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
